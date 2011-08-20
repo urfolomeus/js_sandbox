a13c67579f1f0d34db8a41e66aba15d08628515a
Alan Gardner <urfolomeus@gmail.com>
2011-08-20 19:51:20 +0100

    Some refactoring.

--- javascripts/application.js
+++ javascripts/application.js
@@ -1,21 +1,21 @@
-jQuery(document).ready(function() {
-  jQuery('.preview img').click(function(e){
+jQuery(document).ready(function () {
+  jQuery('.preview img').click(function (e) {
 
     var containerDimensions,
         success = true;
-        minifyContainer = function() {
+        minifyContainer = function () {
           containerDimensions = { height: 528, width: 590 };
           jQuery('#slideshow').removeClass('normal').addClass('mini');
         },
-        normaliseContainer = function() {
+        normaliseContainer = function () {
           containerDimensions = { height: 620, width: 738 };
           jQuery('#slideshow').removeClass('mini').addClass('normal');
         },
-        viewportBiggerThan = function(width, height) {
+        viewportBiggerThan = function (width, height) {
           return jQuery(window).width() > width &&
                  jQuery(window).height() > height;
         },
-        showSlideshow = function() {
+        showSlideshow = function () {
           updatePager();
           updateBlurb();
           jQuery('.overlay').modal({
@@ -44,7 +44,7 @@ jQuery(document).ready(function() {
     }
   });
 
-  previous = function() {
+  previous = function () {
     currentPhoto = $('.current')[0];
     photoStack = $('.stack .photo');
     index = $(photoStack).index(currentPhoto);
@@ -57,7 +57,7 @@ jQuery(document).ready(function() {
     }
   };
 
-  next = function() {
+  next = function () {
     currentPhoto = $('.current')[0];
     photoStack = $('.stack .photo');
     index = $(photoStack).index(currentPhoto);
@@ -70,7 +70,7 @@ jQuery(document).ready(function() {
     }
   };
 
-  first = function() {
+  first = function () {
     currentPhoto = $('.current')[0];
     firstPhoto = $('.stack .photo')[0];
     $(currentPhoto).removeClass('current');
@@ -79,8 +79,8 @@ jQuery(document).ready(function() {
     updateBlurb();
   };
 
-  last = function() {
-    currentPhoto = $('.current')[0];
+  last = function () {
+    currentPhoto = $('.current');
     photoStack = $('.stack .photo');
     length = $(photoStack).length;
     lastPhoto = $(photoStack)[length-1];
@@ -90,8 +90,8 @@ jQuery(document).ready(function() {
     updateBlurb();
   };
 
-  updatePager = function() {
-    currentPhoto = $('.current')[0];
+  updatePager = function () {
+    currentPhoto = $('.current');
     photoStack = $('.stack .photo');
     currentPhotoNum = $(photoStack).index(currentPhoto) + 1;
     length = $(photoStack).length;
@@ -99,64 +99,63 @@ jQuery(document).ready(function() {
     setButtonImages();
   };
 
-  updateBlurb = function() {
-    caption = $('.photo.current .caption')[0];
+  updateBlurb = function () {
+    caption = $('.photo.current .caption');
     $('.blurb').text( $(caption).text() );
   }
 
-  hasPrevious = function() {
-    currentPhoto = $('.current')[0];
+  // navigation
+
+  hasPrevious = function () {
+    currentPhoto = $('.current');
     photoStack = $('.stack .photo');
     currentIndex = $(photoStack).index(currentPhoto);
     return currentIndex > 0;
   }
 
-  hasNext = function() {
-    currentPhoto = $('.current')[0];
+  hasNext = function () {
+    currentPhoto = $('.current');
     photoStack = $('.stack .photo');
-    length = $(photoStack).length;
     currentIndex = $(photoStack).index(currentPhoto);
+    length = $(photoStack).length;
     return currentIndex < length-1;
   }
 
-  setButtonImages = function() {
-    first_button = $('.subnav img.first')[0];
-    $(first_button).attr('src', function() {
-      return hasPrevious() ? 'images/start_sm.jpg' : 'images/start_gray_sm.jpg';
-    });
-    prev_sm_button = $('.subnav img.prev')[0];
-    $(prev_sm_button).attr('src', function() {
-      return hasPrevious() ? 'images/prev_sm.jpg' : 'images/prev_gray_sm.jpg';
-    });
+  setButtonImages = function () {
+    $.fn.setButtonImage = function (isOn, onImage, offImage) {
+      $(this).attr('src', function () {
+        return isOn ? onImage : offImage;
+      });
+    };
 
-    prev_button = $('.nav img.prev')[0];
-    $(prev_button).attr('src', function() {
-      return hasPrevious() ? 'images/previous.jpg' : 'images/previous_gray.jpg';
-    });
-    next_button = $('.nav img.next')[0];
-    $(next_button).attr('src', function() {
-      return hasNext() ? 'images/next.jpg' : 'images/next_gray.jpg';
-    });
+    $.fn.setPreviousImage = function (onImage, offImage) {
+      $(this).setButtonImage(hasPrevious(), onImage, offImage);
+    };
 
-    next_sm_button = $('.subnav img.next')[0];
-    $(next_sm_button).attr('src', function() {
-      return hasNext() ? 'images/next_sm.jpg' : 'images/next_gray_sm.jpg';
-    });
-    last_button = $('.subnav img.last')[0];
-    $(last_button).attr('src', function() {
-      return hasNext() ? 'images/end_sm.jpg' : 'images/end_gray_sm.jpg';
-    });
+    $.fn.setNextImage = function (onImage, offImage) {
+      $(this).setButtonImage(hasNext(), onImage, offImage);
+    };
+
+    // nav
+    $('.nav img.prev').setPreviousImage('images/previous.jpg', 'images/previous_gray.jpg');
+    $('.nav img.next').setNextImage('images/next.jpg', 'images/next_gray.jpg');
+
+    // subnav
+    $('.subnav img.first').setPreviousImage('images/start_sm.jpg', 'images/start_gray_sm.jpg');
+    $('.subnav img.prev').setPreviousImage('images/prev_sm.jpg', 'images/prev_gray_sm.jpg');
+    $('.subnav img.next').setNextImage('images/next_sm.jpg', 'images/next_gray_sm.jpg');
+    $('.subnav img.last').setNextImage('images/end_sm.jpg', 'images/end_gray_sm.jpg');
   }
 
   jQuery('img.first').click(first);
   jQuery('img.prev').click(previous);
   jQuery('img.next').click(next);
   jQuery('img.last').click(last);
-  jQuery('.stack .photo, .nav .prev, .nav .next').mouseover(function() {
+  jQuery('.stack .photo, .nav .prev, .nav .next').mouseover(function () {
     $('.nav .prev').show();
     $('.nav .next').show();
   });
-  jQuery('.stack .photo, .nav .prev, .nav .next').mouseout(function() {
+  jQuery('.stack .photo, .nav .prev, .nav .next').mouseout(function () {
     $('.nav .prev').hide();
     $('.nav .next').hide();
   });
